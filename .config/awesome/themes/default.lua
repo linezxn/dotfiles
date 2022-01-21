fec5339eff855cee096ba4fff6b8e67cc651345c
linezxn <pinglantinfeng@gmail.com>
2022-01-21 12:30:00 +0800

    first commit

---
+++ .config/awesome/themes/default.lua
@@ -0,0 +1,16 @@
+-- Default
+------------------
+local gears = require('gears')
+local theme = {}
+
+theme.font          = 'JetBrains Mono 10'
+
+theme.useless_gap   = 7
+theme.border_width  = 0
+theme.border_normal = "#151515"
+theme.border_focus  = "#151515"
+theme.border_marked = "#151515"
+theme.snap_bg = "#E8E3E3"
+theme.snap_shape = gears.shape.rectangle
+
+return theme
