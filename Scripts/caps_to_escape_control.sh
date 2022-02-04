3ea48b09881ca29af4f7da0630f8db58550e6690
linezxn <pinglantinfeng@gmail.com>
2022-02-05 04:34:54 +0800

    add fonts and Scripts

---
+++ Scripts/caps_to_escape_control.sh
@@ -0,0 +1,3 @@
+setxkbmap -option ctrl:nocaps
+setxkbmap -option ctrl:swapcaps
+xcape -e 'Control_L=Escape'
---
+++ Scripts/install.sh
@@ -0,0 +1,3 @@
+sudo xbps-install awesome rofi dunst polybar p7zip firefox kitty neovim gcc pulseaudio xbacklight xrandr linx-firmware-intel llvm clang clang-tools-extra xcape setxkbmap zsh void-repo-nonfree ffmpeg
+
+xorg-minimal mesa-dri intel-media-driver 
